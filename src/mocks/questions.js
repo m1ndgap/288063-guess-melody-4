c65a23c00834ecf627b4318316f31af2b6613a80
Tim Chumbash <posseslash@gmail.com>
2020-06-22 13:45:21 +0200

    writing mocks, installed react dom router

---
+++ src/mocks/questions.js
@@ -0,0 +1,15 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/128`
+
+export default [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: ``,
+      genre: ``,
+    }, {
+      src: ``,
+      genre: ``,
+    }]
+  }
+]
